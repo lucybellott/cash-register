96adf4d475005e5814e28041f88ffccd6ad675d9
Lucy <lucybellott@gmail.com>
2024-04-12 19:02:43 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Design and build a cash register that can:
+
+// Add and remove items
+// Counts the amount of coins and bills in the drawer
+// Sum the total amount of money in the drawer
+// Determine whether it is possible to create a specific cash amount from the current items in the drawer
+// Computes the change required from a transaction and removes it from the drawer if possible.
+
+
+//Removes a single item from the drawer
+function removeItem(name, drawer){
+
+}
+
+//Adds a single item to the drawer
+function addItem(name, drawer){
+    
+}
